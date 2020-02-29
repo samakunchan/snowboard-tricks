0961ea3c4cf256a43fbb1b19f2a8166ad909a1a9
Samakunchan <samakunchan@gmail.com>
2020-02-29 03:28:17 +0100

    feat: crud comment
    - Ajout du CommentController.php
    - Ajout du CommentType.php
    - Ajout des templates comment
    - Modification de l'entity Comment.php pour les commentaires signalés et modérés
    - Modification de l'entity User.php pour la propriété comments en OneToMany
    - Modification de l'entity Comment.php pour la propriété user en ManyToOne

--- assets/js/add-image.js
+++ assets/js/add-image.js
@@ -1,18 +1,5 @@
 $(document).ready(function() {
-    const $containerImg = $("div#trick_images");
-    let indexImg = $containerImg.find(":input").length;
-    $("#add_image").click(function(e) {
-        addImage($containerImg);
-        e.preventDefault();
-        return false;
-    });
-    if (indexImg === 0) {
-        addImage($containerImg);
-    } else {
-        $containerImg.children("div").each(function() {
-            addDeleteLink($(this));
-        });
-    }
+
     function addImage($containerImg) {
         const template = $containerImg.attr("data-prototype")
             .replace(/__name__label__/g, "")
@@ -27,10 +14,29 @@ $(document).ready(function() {
     function addDeleteLink($prototype) {
         const $deleteLink = $("<a href=\"javascript:void(0)\" class=\"btn btn-danger\">Supprimer</a>");
         $prototype.append($deleteLink);
+
         $deleteLink.click(function(e) {
             $prototype.remove();
             e.preventDefault();
             return false;
         });
     }
+    const trickImages = $("div#trick_images");
+    let indexImg = trick_images.find(":input").length;
+    $("#add_image").click(function(e) {
+
+        addImage(trickImages);
+        e.preventDefault();
+        return false;
+    });
+    if (indexImg === 0) {
+        addImage(trickImages);
+
+    } else {
+        trickImages.children("div").each(function() {
+
+            addDeleteLink($(this));
+        });
+    }
+
 });
--- assets/js/edit-image.js
+++ assets/js/edit-image.js
@@ -1,40 +1,70 @@
 $(document).ready(function() {
-    $.each($(".image-edit"),function (index) {
-        console.log("trick_images_" + index);
-        const $containerImg = $("#trick_images_" + index);
-        let indexImg = $containerImg.find(":input").length;
-        $("#add_image").click(function(e) {
-            addImage($containerImg);
-            e.preventDefault();
-            return false;
-        });
-        if (indexImg === 0) {
-            addImage($containerImg);
-        } else {
-            $containerImg.children("div").each(function() {
-                addDeleteLink($(this), index);
-            });
-        }
-        function addImage($containerImg) {
-            const template = $("<div id=\"trick_image_"+(index + 1)+"\"><div><input type=\"file\" id=\"trick_images_"+(index + 1)+"_file\" name=\"trick[images]["+(index + 1)+"][file]\"" + " required=\"required\"></div></div>");
+    let count = [];
+    function addNewImage(container) {
+        const template = $(""+"<div class=\"previous-img-"+count.length+"\"></div>");
+        const subTemplate = $("<div id=\"trick_image_"+count.length+"\" class=\"image-edit\">" +
+            "<div>" +
+            "<input type=\"file\" id=\"trick_images_"+count.length+"_file\"" + " name=\"trick[images]["+count.length+"][file]\"" + " required=\"required\">" +
+            "</div>" +
+            "</div>");
+        template.append(subTemplate);
+        container.append(template);
+        count.push(subTemplate);
+    }
 
-            const $prototype = $(template);
-            addDeleteLink($prototype, index);
-            $containerImg.append($prototype);
-            indexImg++;
-        }
-    });
-
-
-    function addDeleteLink($prototype, index) {
-        console.log(index);
+    function addNewDeleteLink($prototype) {
         const $deleteLink = $("<a href=\"javascript:void(0)\" class=\"btn btn-danger\">Supprimer</a>");
-        $prototype.append($deleteLink);
-        console.log(".previous-img-"+index, $(".previous-img-"+index));
+        $prototype[count.length-1].append($deleteLink[0]);
         $deleteLink.click(function(e) {
-            $(".previous-img-"+index).remove();
+            $(".previous-img-"+ (count.length-1)).remove();
             e.preventDefault();
+            const start = count.length-1;
+            count.splice(start, 1);
             return false;
         });
     }
+
+    function addImagePrevious($containerImg, index) {
+        const template = $("<div id=\"trick_image_"+(index + 1)+"\"><div><input type=\"file\" id=\"trick_images_"+(index + 1)+"_file\" name=\"trick[images]["+(index + 1)+"][file]\"" + " required=\"required\"></div></div>");
+
+        const $prototype = $(template);
+        addDeletePreviousLink($prototype, index);
+        $containerImg.append($prototype);
+    }
+    function addDeletePreviousLink($prototype, index) {
+        const $deleteLink = $("<a href=\"javascript:void(0)\" class=\"btn btn-danger\">Supprimer</a>");
+        $prototype.append($deleteLink);
+        $deleteLink.click(function(e) {
+            $(".previous-img-"+index).remove();
+            e.preventDefault();
+            const start = count.length-1;
+            count.splice(start, 1);
+            return false;
+        });
+    }
+
+    $.each($(".image-edit"),function (index) {
+        const trickImagesLoop = $("#trick_images_" + index);
+
+        let indexImg = trickImagesLoop.find(":input").length;
+        if (indexImg === 0) {
+            addImagePrevious($("#trick_images_0"), index);
+        } else {
+            trickImagesLoop.children("div").each(function() {
+                addDeletePreviousLink($(this), index);
+            });
+        }
+        count.push($(".image-edit"));
+    });
+
+
+
+    $("#add_image").click(function(e) {
+        addNewImage($(".form-image"));
+        addNewDeleteLink($(".image-edit"));
+        e.preventDefault();
+        return false;
+    });
+
+
 });
